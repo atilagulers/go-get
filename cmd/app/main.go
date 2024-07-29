1572ae3fb44a80ab6e819a77d897a1895d56251b
Atila Guler <128936466+atilagulers@users.noreply.github.com>
2024-07-29 03:11:16 +0300

    .

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -12,7 +14,23 @@ func main() {
 	// logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		res, err := http.Get("https://api.trendyol.com/grocerygw/categories")
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+
+		var jsonResponse []map[string]any
+		if err := json.Unmarshal(body, &jsonResponse); err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, jsonResponse)
 	})
 
 	e.Logger.Info("Server started on port 8080")
